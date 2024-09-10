fcc8e42b28603216ac84e71936c8943495d4f507
kismatkc <kismatkc28@gmail.com>
2024-09-10 12:29:05 -0400

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return;
+  <>
+    <header>
+      <div></div>
+      <div></div>
+      <div></div>
+    </header>
+    <main></main>
+    <footer></footer>
+  </>;
+}
